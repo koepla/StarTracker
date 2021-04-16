98f0b18de74ad99023f357d677dada9c0250b2d8
Elias <40146720180223@litec.ac.at>
2021-04-17 00:39:06 +0200

    implemented first version of our microcontroller protocol
    stt_protocol - for documentation read the comments

---
+++ test/microcontroller/protocol/stt_protocol.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <vector>
+
+/* 
+*	enumeration for protocol flags 
+*/
+enum class stt_flag {
+	
+	NONE = -1,
+	WAKEUP = 0,
+	SLEEP = 1,
+	MOVE = 2,
+	STOP = 3,
+	ORIGIN = 4,
+	ACK
+};
+
+/*
+*	header struct, contains flag and size in bytes 
+*/
+struct stt_header {
+
+	stt_flag flag;
+	uint32_t size;
+
+	stt_header() {
+
+		this->flag = stt_flag::NONE;
+		this->size = 0;
+	}
+	stt_header(stt_flag flag, uint32_t size) {
+
+		this->flag = flag;
+		this->size = size;
+	}
+};
+
+/* 
+*	protocol struct, contains header and a byte buffer, which size can be specified by the template
+*/
+template <int n>
+struct stt_protocol {
+
+	stt_header header;
+	uint8_t buff[n];
+
+	/* 
+	*	Sets the header flag to NONE and the size to zero
+	*	Sets the whole buffer to 0
+	*/
+	stt_protocol() {
+		this->header = stt_header(stt_flag::NONE, 0);
+		std::memset(buff, 0, n);
+	}
+
+	/*
+	*	Sets the specified header flag
+	*	Sets size and buffer to zero
+	*/
+	stt_protocol(stt_flag flag) {
+
+		this->header = stt_header(flag, 0);
+		std::memset(buff, 0, n);
+	}
+
+	/*
+	*	Function for pushing data into the buffer, 
+	*	Takes data and element count
+	*	Copes data into the buffer
+	*/
+	template <typename T>
+	stt_protocol& push(const T& data, uint32_t count) {
+
+		uint32_t osize = this->header.size;
+		std::memcpy(buff + osize, &data, count * sizeof(T));
+		this->header.size += count * sizeof(T);
+
+		return *this;
+	}
+
+	/*
+	*	Returns the size in bytes of the whole struct
+	*/
+	uint32_t size() const {
+		return sizeof(this->header) + this->header.size;
+	}
+};
+
+/*
+*	Helper union if a byte buffer for the stt_protocol struct is needed
+*/
+template <int n>
+union stt_protocol_bytes {
+
+	stt_protocol<n> transfer;
+	uint8_t buff[n + sizeof(stt_header)];
+
+	stt_protocol_bytes() {
+		this->transfer = stt_protocol<n>();
+	}
+
+	stt_protocol_bytes(stt_protocol<n> transfer) {
+		this->transfer = transfer;
+	}
+};
+
+int main(int argc, char** argv) {
+
+	constexpr int BUFF_SIZE = 2048;
+	stt_protocol<BUFF_SIZE> transfer(stt_flag::ACK);
+	transfer.push<int>(100, 1);
+	transfer.push<int>(200, 1);
+	transfer.push<char>('a', 1);
+
+	int* ptr = reinterpret_cast<int*>(&transfer);
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << ptr[i] << std::endl;
+	}
+	return 0;
+}
