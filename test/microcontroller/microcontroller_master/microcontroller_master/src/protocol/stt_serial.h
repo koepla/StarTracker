e54da1b5fa664ea0dadb69875a0ca72e8624e18c
Elias <40146720180223@litec.ac.at>
2021-04-17 17:22:27 +0200

    added comments

--- test/microcontroller/microcontroller_master/microcontroller_master/src/protocol/stt_serial.h
+++ test/microcontroller/microcontroller_master/microcontroller_master/src/protocol/stt_serial.h
@@ -13,13 +13,12 @@ private:
 public:
 
 	stt_serial_exception(std::string&& message) noexcept : message(message) {
-		
+			
 	}
 
 	const char* what() const noexcept override {
 		return this->message.c_str();
 	}
-
 };
 
 class stt_serial {
@@ -35,6 +34,10 @@ public:
 		this->isopen = false;
 	}
 
+	/*
+	*	Opens the specified port
+	*	Arguments are portname and baudrate
+	*/
 	void open(const std::string& port, uint32_t baudrate) noexcept(false) {
 
 		std::string prefixed = this->prefix_port(port);
@@ -52,12 +55,19 @@ public:
 		this->set_baudrate(baudrate);
 	}
 
+	/*
+	*	Closes the currently open Port
+	*/
 	void close() noexcept {
 
 		CloseHandle(this->hcomm);
 		this->isopen = false;
 	}
 
+	/*
+	*	Reads from currently open Port
+	*	Takes byte-buffer and bytes to reader
+	*/
 	uint32_t read(uint8_t* buffer, uint32_t bytes2read) noexcept(false) {
 
 		// Number of bytes read
@@ -78,14 +88,21 @@ public:
 		}		
 	}
 
+	/*
+	*	Writes to currently open Port
+	*	Takes byte-buffer and bytes to write
+	*/
 	uint32_t write(uint8_t* buffer, uint32_t bytes2write) noexcept(false) {
 
+		// Number of bytes written
 		DWORD dwwritten;
 
+		// Check if port is open
 		if (!this->is_open()) {
 			throw stt_serial_exception("Port is not open");
 		}
 
+		// Write operation
 		if (!WriteFile(this->hcomm, reinterpret_cast<LPVOID>(buffer), static_cast<DWORD>(bytes2write), &dwwritten, NULL)) {
 
 			throw stt_serial_exception("Couldn't write to port");
@@ -95,12 +112,18 @@ public:
 		}
 	}
 
+	/*
+	*	Returns true if Port is open
+	*/
 	bool is_open() const noexcept {
 		return this->isopen;
 	}
 
 private:
 
+	/*
+	*	Add Portname Prefix if needed
+	*/
 	inline std::string prefix_port(const std::string& port) {
 
 		if (strncmp(port.c_str(), "\\\\.\\", 4) == 0) {
@@ -113,6 +136,9 @@ private:
 		}
 	}
 
+	/*
+	*	Set Baudrate via CommState
+	*/
 	void set_baudrate(uint32_t baudrate) {
 
 		DCB dcb = { 0 };
