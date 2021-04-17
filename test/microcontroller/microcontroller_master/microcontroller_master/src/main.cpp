d86e513ac6c99788913b960d0ba828390fbf40bd
Elias <40146720180223@litec.ac.at>
2021-04-17 19:13:22 +0200

    changed test environment

--- test/microcontroller/microcontroller_master/microcontroller_master/src/main.cpp
+++ test/microcontroller/microcontroller_master/microcontroller_master/src/main.cpp
@@ -5,16 +5,40 @@
 int main() {
 
 	stt_serial serial;
+	uint8_t rbuff[10];
 
 	try {
 		serial.open("COM4", 9600);
+
+		int i = 0;
+
+		while (true) {
+
+			if (serial.rx_available()) {
+				
+				serial.read(rbuff, 10);
+
+				for (int i = 0; i < 10; i++) {
+					std::cout << (int)rbuff[i] << " ";
+				}
+				std::cout << std::endl;
+			}
+			else {
+				std::cout << "not" << std::endl;
+			}
+		}
+
 		serial.close();
 	}
 	catch (const stt_serial_exception& e) {
-		std::cerr << e.what() << std::endl;
+
+		std::cerr << e.what() << "Error Code: " << GetLastError() <<  std::endl;
+		exit(-1);
 	}
 	catch (const std::exception& e) {
-		std::cerr << e.what() << std::endl;
+
+		std::cerr << e.what() << "Error Code: " << GetLastError() << std::endl;
+		exit(-1);
 	}
 	
 	return 0;
