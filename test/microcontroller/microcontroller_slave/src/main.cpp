e794dffdf3c19762016a129327f6515d65bbe471
Elias <40146720180223@litec.ac.at>
2021-08-17 14:24:35 +0200

    config update

--- test/microcontroller/microcontroller_slave/src/main.cpp
+++ test/microcontroller/microcontroller_slave/src/main.cpp
@@ -28,6 +28,7 @@ DriverConfig yawConf = {
     .txPin = 12
 };
 
+// only for plank
 DriverConfig singleConf = {
 
     .microSteps = 256,
@@ -37,7 +38,7 @@ DriverConfig singleConf = {
     .txPin = 6
 };
 
-Driver driver = Driver(pitchLeftConf, pitchRightConf, singleConf, 600 /* rms current */);
+Driver driver = Driver(pitchLeftConf, pitchRightConf, yawConf, 600 /* rms current */);
 Protocol::Pack64 package;
 
 void setup(){           
