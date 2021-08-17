64a70863dcd211dfda8f910d7cf2c2ad3c0039ad
Elias <40146720180223@litec.ac.at>
2021-08-17 12:48:04 +0200

    Refactored the Serial class

--- test/microcontroller/microcontroller_master/microcontroller_master/src/protocol/Serial.hpp
+++ test/microcontroller/microcontroller_master/microcontroller_master/src/protocol/Serial.hpp
@@ -8,80 +8,82 @@
 *	For reference, see https://docs.microsoft.com/en-us/previous-versions/ff802693(v=msdn.10)?redirectedfrom=MSDN
 */
 
-namespace protocol {
+namespace Protocol {
 
-	class stt_serial_exception : public std::exception {
+	class SerialException : public std::exception {
 
 	private:
 		std::string message;
 
 	public:
-		stt_serial_exception(std::string&& message) noexcept;
-		const char* what() const noexcept override;
 
+		SerialException(std::string&& message) noexcept;
+
+		const char* what() const noexcept override;
 	};
 
-	class stt_serial
+	class Serial
 	{
 	private:
-		HANDLE hcomm;
-		DWORD dweventmask;
-		bool isopen;
+		HANDLE hCom;
+		DWORD dwEventMask;
+		bool isOpen;
 
 	public:
 
-		stt_serial();
-		~stt_serial();
+		Serial();
+		~Serial();
 
 		/*
 		*	Opens the specified port
 		*	Arguments are portname and baudrate
 		*/
-		void open(const std::string& port, uint32_t baudrate) noexcept(false);
+		void Open(const std::string& port, uint32_t baudrate) noexcept(false);
 
 		/*
 		*	Closes the currently open Port
 		*/
-		void close() noexcept(false);
+		void Close() noexcept(false);
 
 		/*
 		*	Reads from currently open Port
 		*	Takes byte-buffer and bytes to reader
 		*/
-		uint32_t read(uint8_t* buffer, uint32_t bytes2read) noexcept(false);
+		uint32_t Read(uint8_t* buffer, uint32_t bytes2read) noexcept(false);
 
 		/*
 		*	Writes to currently open Port
 		*	Takes byte-buffer and bytes to write
 		*/
-		uint32_t write(uint8_t* buffer, uint32_t bytes2write) noexcept(false);
+		uint32_t Write(uint8_t* buffer, uint32_t bytes2write) noexcept(false);
 
 		/*
 		*	Returns true if Port is open
 		*/
-		bool is_open() const noexcept;
-		bool rx_available() noexcept;
+		bool IsOpen() const noexcept;
+
+		bool RxAvailable() noexcept;
 
 	private:
 
 		/*
 		*	Add Portname Prefix if needed
 		*/
-		inline std::string prefix_port(const std::string& port);
+		[[nodiscard]] inline std::string prefixPort(const std::string& port);
 
 		/*
 		*	Set Baudrate via CommState
 		*/
-		void set_baudrate(uint32_t baudrate);
-		void init_recievemask();
-		void init_commtimeout();
+		void setBaudrate(uint32_t baudrate);
+		void initReceiveMask();
+		void initTimeouts();
 
 	public:
 
 		/*
 		*	Returns a list of all available ports
 		*/
-		static std::vector<std::string> get_ports();
+		[[nodiscard]] static std::vector<std::string> GetPortNames();
 
 	};
 }
