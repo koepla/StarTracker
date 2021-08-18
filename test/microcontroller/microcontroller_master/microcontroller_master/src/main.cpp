41e358d85ea55ea4921002e23dea7536f94fc342
Elias <40146720180223@litec.ac.at>
2021-08-18 11:36:59 +0200

    removed comment

--- test/microcontroller/microcontroller_master/microcontroller_master/src/main.cpp
+++ test/microcontroller/microcontroller_master/microcontroller_master/src/main.cpp
@@ -3,24 +3,6 @@
 #include "protocol/Serial.hpp"
 #include "util/Hardware.hpp"
 
-/*
-*	TODO: Figure out a solution for the following problem
-*	
-*	The Arduino only has a Serial Buffer of 64 bytes, thus the maximum byte
-*	buffer size of our protocol would be 52 or 56, because we have to acount for the
-*	8 byte header. Now we have to decide, whether the byte buffer size should be 
-*	constant (52 / 56), or still modifyable, because packages like stop and origin 
-*	have zero payload other than the header. We also have to acount for the handling 
-*	on the microcontroller side, because if we have data larger than 52 / 56 bytes, 
-*	we have to send it in a series of packages, for which we would have to
-*	introduce a new flag, which would be dependent on the previous flag. One might argue that
-*	this new flag could only occur when sending data for the MOVE flag, which is true, but
-*	you never know. We also have to implement functions on the Arduino side for dealing with 
-*	incoming data. We could use a global data buffer, in which all of the payload is loaded.
-*	Such a design would not be good, because in theory, when the move flag is set, the arduino
-*	should start turning the stepper motors.
-*/
-
 std::string autoDetectPort(Protocol::Serial& port);
 
 int main(int argc, char** argv) {
