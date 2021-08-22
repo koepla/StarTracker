cfb3abe6eba1dc8008ea38f3361ac30f9302813b
Elias <40146720180223@litec.ac.at>
2021-08-22 17:48:57 +0200

    Added StarTracker Solution
    StarAPI builds to static library, referenced by StarTracker, StarTracker sends computed data to microcontroller

---
+++ src/StarTracker/StarAPI/StarAPI/StarAPI.hpp
@@ -0,0 +1,12 @@
+#ifndef _STAR_API_H_
+#define _STAR_API_H_
+
+#include "Date.hpp"
+#include "Math.hpp"
+#include "coordinates/Horizontal.hpp"
+#include "coordinates/Rectangular.hpp"
+#include "coordinates/Spherical.hpp"
+#include "coordinates/Terrestial.hpp"
+#include "coordinates/Transform.hpp"
+
+#endif
