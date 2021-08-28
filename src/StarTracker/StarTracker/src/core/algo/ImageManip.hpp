4a0e300db56e0ce8e79bc2ca9a244231a0e7013e
Ilja Köhler <45200001+KI20180151@users.noreply.github.com>
2021-08-28 20:51:46 +0200

    created ferri sipmPle image stacking function

---
+++ src/StarTracker/StarTracker/src/core/algo/ImageManip.hpp
@@ -0,0 +1,27 @@
+#ifndef _IMAGEMANIP_H_
+#define _IMAGEMANIP_H_
+
+#include <vector>
+
+namespace Algo {
+
+	class ImageManip {
+
+	public:
+
+		template <typename T>
+		static std::vector<T> Stack(const T* img1, const T* img2, uint32_t size) {
+
+			std::vector<T> out(size);
+
+			for (auto i = 0; i < size; i++) {
+
+				out[i] = (((*(img1 + i)) + (*(img2 + i))) / 2);
+			}
+
+			return out;
+		}
+	};
+}
+
+#endif // _IMAGEMANIP_H_
