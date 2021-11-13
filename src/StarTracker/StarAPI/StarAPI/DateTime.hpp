b10b5f53b19e0b6150660cd9b78d61d132436751
Elias <45200003+plankElias@users.noreply.github.com>
2021-11-13 20:42:22 +0100

    added code for adding and subtracting parts of datetime

--- src/StarTracker/StarAPI/StarAPI/DateTime.hpp
+++ src/StarTracker/StarAPI/StarAPI/DateTime.hpp
@@ -24,12 +24,23 @@ namespace StarTracker {
 		DateTime(int64_t year, int64_t month, int64_t day, int64_t hour, int64_t minute, int64_t second) noexcept;
 
 	public:
+		void AddYears(int64_t years) noexcept;
+		void AddMonths(int64_t months) noexcept;
+		void AddDays(int64_t days) noexcept;
+		void AddHours(int64_t hours) noexcept;
+		void AddMinutes(int64_t minutes) noexcept;
+		void AddSeconds(int64_t seconds) noexcept;
 
+	private:
+		void addDays(int64_t days, const int monthDays) noexcept;
+
+	public:
 		/*
 		*	returns the current date
 		*/
 		[[nodiscard]] static DateTime Now() noexcept;
 		[[nodiscard]] static DateTime UtcNow() noexcept;
+		[[nodiscard]] static int64_t UtcDiff() noexcept;
 
 		/*
 		*	calculates the julian day number for a given date
@@ -49,11 +60,7 @@ namespace StarTracker {
 
 		/*
 		*	Local mean sidereal time in degrees
-		* 
-		*	Caution: This may still be an issue, it works for now
 		*/
-
-#pragma message ("Temporary fix for Gmst, because we would only be allowed to pass the relative UTC time. This may still produce some unknown issues.") 
 		[[nodiscard]] static double Gmst(const DateTime& utc) noexcept;
 
 		/*
