97dfd0f8391dd57bfa26a3299a35ae458a18cfee
Elias <40146720180223@litec.ac.at>
2022-01-14 11:36:06 +0100

    update work on application

--- src/StarTracker/StarTracker/src/core/Application.cpp
+++ src/StarTracker/StarTracker/src/core/Application.cpp
@@ -2,28 +2,66 @@
 
 namespace StarTracker::Core {
 
-	Application::Application(const ApplicationData& applicationData) noexcept : window{ applicationData } {
-
+	Application::Application(const ApplicationData& applicationData) noexcept : 
+		window{ 
+			WindowData { 
+				.Width = applicationData.Width,
+				.Height = applicationData.Height,
+				.Title = applicationData.Title,
+				.Running = true,
+				.Fullscreen = applicationData.Fullscreen
+			} 
+		}, 
+		userInterfaceView{ nullptr, applicationData.EnableDockspace } 
+	{
+		viewList = std::vector<View*>{};
+		userInterfaceView.SetNativeWindowHandle(window.GetNativeHandle());
 		instance = this;
 	}
 
 	void Application::Run() noexcept {
 
-		float lastFrameTime = 0.0f;
+		userInterfaceView.OnInit();
+		for (auto* view : viewList) {
+
+			view->OnInit();
+		}
+
+		float lastFrameTime = glfwGetTime();
 
 		while (window.IsRunning()) {
+
+			glClear(GL_COLOR_BUFFER_BIT);
 
 			float time = glfwGetTime();
 			float deltaTime = time - lastFrameTime;
 			lastFrameTime = time;
 
+			userInterfaceView.UIBegin();
+			for (auto* view : viewList) {
+
+				view->OnUpdate(deltaTime);
+			}
+			userInterfaceView.UIEnd();
+
 			window.Update();
 		}
+
+		for (auto* view : viewList) {
+
+			view->OnDestroy();
+		}
+		userInterfaceView.OnDestroy();
 	}
 
 	void Application::Close() noexcept {
 
 		window.GetWindowData().Running = false;
+	}
+
+	void Application::AddToViewList(View* view) noexcept {
+
+		viewList.emplace_back(view);
 	}
 
 	Window& Application::GetWindow() noexcept {
