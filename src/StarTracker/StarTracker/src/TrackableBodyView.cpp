ad9aa6634b39845e5cc0559b89800dec7947c09d
Elias Engelbert Plank <40146720180223@litec.ac.at>
2022-01-16 12:32:01 +0100

    view for sample app

---
+++ src/StarTracker/StarTracker/src/TrackableBodyView.cpp
@@ -0,0 +1,81 @@
+#include "TrackableBodyView.hpp"
+
+namespace StarTracker {
+
+	TrackableBodyView::TrackableBodyView(void* nativeWindowHandle) noexcept : Core::View{ nativeWindowHandle } {
+	
+		observer = {};
+		celestialBodies = {};
+	}
+
+	void TrackableBodyView::OnInit() noexcept {
+
+		celestialBodies = Ephemeris::CelestialBody::LoadFromFile("assets/ephemeris/CelestialBodies.json");
+		observer = [&]() -> Ephemeris::Coordinates::Observer {
+
+			try {
+
+				auto geoLocation = Utils::LocationService::GeoLocation::Get();
+
+				Ephemeris::Coordinates::Observer observer{};
+				observer.Latitude = geoLocation.Latitude;
+				observer.Longitude = geoLocation.Longitude;
+				return observer;
+			}
+			catch (const std::exception&) {
+
+				return {};
+			}
+		}();
+	}
+
+	void TrackableBodyView::OnUpdate(float deltaTime) noexcept {
+
+		if (ImGui::Begin("Trackable Bodies")) {
+
+			ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[Core::UIFont::Medium]);
+			ImGui::Text("%s", DateTime::Now().ToString().c_str());
+			ImGui::PopFont();
+			ImGui::Separator();
+			
+			for (const auto& body : celestialBodies) {
+
+				const auto position = Ephemeris::Coordinates::Transform::TerrestrialObserverToHorizontal(
+					body->GetSphericalPosition(DateTime::Now()),
+					observer,
+					DateTime::Now()
+				);
+
+				const auto data = std::format("Name: {}, Designation: {}, Azimuth: {}, Elevation: {}", 
+					body->GetName(),
+					body->GetDesignation(),
+					position.Azimuth,
+					position.Altitude
+				);
+
+				const auto windowId = std::format("Begin Tracking {} ({})", body->GetName(), body->GetDesignation());
+
+				bool selected = false;
+				if (ImGui::Selectable(data.c_str(), &selected)) {
+
+					ImGui::OpenPopup(windowId.c_str());					
+				}
+
+				bool popUpOpen = true;
+				if (ImGui::BeginPopupModal(windowId.c_str(), &popUpOpen)) {
+
+					ImGui::Text("Tracking %s", body->GetName().c_str());
+					ImGui::EndPopup();
+				}
+				ImGui::Separator();
+			}
+		}
+		
+		ImGui::End();
+	}
+
+	void TrackableBodyView::OnDestroy() noexcept {
+
+
+	}
+}
---
+++ src/StarTracker/StarTracker/src/TrackableBodyView.hpp
@@ -0,0 +1,36 @@
+#ifndef STARTRACKER_TRACKABLEBODYVIEW_H
+#define STARTRACKER_TRACKABLEBODYVIEW_H
+
+#include <StarAPI/StarAPI.hpp>
+
+#include "core/Application.hpp"
+#include "core/Assert.hpp"
+#include "core/Core.hpp"
+#include "utils/serial/Package.hpp"
+#include "utils/serial/Serial.hpp"
+
+#include "utils/GeoLocation.hpp"
+#include "utils/HttpRequest.hpp"
+#include "utils/Stopwatch.hpp"
+
+#include <filesystem>
+#include <iostream>
+
+namespace StarTracker {
+
+	class TrackableBodyView : public Core::View {
+
+	private:
+		Ephemeris::Coordinates::Observer observer;
+		std::vector <std::shared_ptr<Ephemeris::CelestialBody>> celestialBodies;
+
+	public:
+		explicit TrackableBodyView(void* nativeWindowHandle) noexcept;
+
+		virtual void OnInit() noexcept override;
+		virtual void OnUpdate(float deltaTime) noexcept override;
+		virtual void OnDestroy() noexcept override;
+	};
+}
+
+#endif // STARTRACKER_TRACKABLEBODYVIEW_H
