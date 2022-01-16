c4d83070eebbcdf1d565b0975a41ee5fd5c27a75
Elias Engelbert Plank <40146720180223@litec.ac.at>
2022-01-16 12:32:17 +0100

    main method for sample app

--- src/StarTracker/StarTracker/src/main.cpp
+++ src/StarTracker/StarTracker/src/main.cpp
@@ -1,71 +1,4 @@
-#include <StarAPI/StarAPI.hpp>
-
-#include "core/Core.hpp"
-#include "core/Assert.hpp"
-#include "core/Application.hpp"
-#include "utils/serial/Package.hpp"
-#include "utils/serial/Serial.hpp"
-
-#include "utils/GeoLocation.hpp"
-#include "utils/HttpRequest.hpp"
-#include "utils/Stopwatch.hpp"
-
-#include <iostream>
-#include <filesystem>
-
-class ComputationView : public StarTracker::Core::View {
-
-private:
-	std::vector<std::shared_ptr<StarTracker::Ephemeris::CelestialBody>> celestialBodies;
-	StarTracker::Ephemeris::Coordinates::Observer observer;
-
-public:
-	explicit ComputationView(void* nativeWindowHandle) noexcept : View{ nativeWindowHandle }, celestialBodies{}, observer{} {
-
-	}
-
-	virtual void OnInit() noexcept override {
-
-		const auto geoLocation = StarTracker::Utils::LocationService::GeoLocation::Get();
-		observer = { geoLocation.Latitude, geoLocation.Longitude };
-		celestialBodies = StarTracker::Ephemeris::CelestialBody::LoadFromFile("assets/ephemeris/CelestialBodies.json");
-	}
-
-	virtual void OnUpdate(float deltaTime) noexcept override {
-
-		ImGui::Begin("Celestial Bodies");
-
-		auto mediumFont = ImGui::GetIO().Fonts->Fonts[StarTracker::Core::UIFont::Medium];
-
-		ImGui::PushFont(mediumFont);
-		ImGui::Text("%s", StarTracker::DateTime::Now().ToString().c_str());
-		ImGui::PopFont();
-
-		ImGui::Separator();
-
-		for (auto& body : celestialBodies) {
-
-			auto position = StarTracker::Ephemeris::Coordinates::Transform::TerrestrialObserverToHorizontal(
-				body->GetSphericalPosition(StarTracker::DateTime::Now()),
-				observer,
-				StarTracker::DateTime::Now()
-			);
-			ImGui::Text("Name: {%s} Designation: {%s} Azimuth: {%lf} Elevation: {%lf}",
-				body->GetName().c_str(),
-				body->GetDesignation().c_str(),
-				position.Azimuth,
-				position.Altitude
-			);
-			ImGui::Separator();
-		}
-		ImGui::End();
-	}
-
-	virtual void OnDestroy() noexcept override {
-
-
-	}
-};
+#include "StarTrackerApplication.hpp"
 
 auto main(int, char**) -> int {
 
@@ -78,11 +11,8 @@ auto main(int, char**) -> int {
 	applicationData.VerticalSync = false;
 
 	try {
-		StarTracker::Core::Application application{ applicationData };
 
-		const auto view = std::make_unique<ComputationView>(application.GetWindow().GetNativeHandle());
-
-		application.AddToViewList(view.get());
+		StarTracker::StarTrackerApplication application{ applicationData };
 		application.Run();
 	}
 	catch (const std::exception& e) {
