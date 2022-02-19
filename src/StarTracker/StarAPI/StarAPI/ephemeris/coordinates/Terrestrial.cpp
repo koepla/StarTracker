85eff8525770b6486d34491c32f0bcdbedd96097
Elias Engelbert Plank <40146720180223@litec.ac.at>
2022-02-19 12:25:47 +0100

    structs should only carry data

--- src/StarTracker/StarAPI/StarAPI/ephemeris/coordinates/Terrestrial.cpp
+++ src/StarTracker/StarAPI/StarAPI/ephemeris/coordinates/Terrestrial.cpp
@@ -2,16 +2,4 @@
 
 namespace StarTracker::Ephemeris::Coordinates {
 
-	Terrestrial::Terrestrial() : Latitude{ 0 }, Longitude{ 0 } {
-
-	}
-
-	Terrestrial::Terrestrial(double latitude, double longitude) : Latitude{ latitude }, Longitude{ longitude } {
-
-	}
-
-	std::string Terrestrial::ToString() const noexcept {
-
-		return "[latitude = " + std::to_string(Latitude) + " | longitude = " + std::to_string(Longitude) + "]";
-	}
 }
