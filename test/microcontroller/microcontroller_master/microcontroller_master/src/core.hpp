b78f34998256147e6821b785aa7c79e19a4a4e4c
Elias Engelbert Plank <40146720180223@litec.ac.at>
2022-02-22 21:10:02 +0100

    remove test folder

--- test/microcontroller/microcontroller_master/microcontroller_master/src/core.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _CORE_H_
-#define _CORE_H_
-
-#include <cstdio>
-
-#define STR_ASSERT(condition, msg) if(!(condition)){ fprintf(stderr, "%s", msg); __debugbreak(); } 
-
-#endif // _CORE_H_
--- test/microcontroller/microcontroller_master/planetary_api/src/Astro.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _ASTRO_H_
-#define _ASTRO_H_
-
-#include "coordinates/Transform.hpp"
-#include "Date.hpp"
-#include "Math.hpp"
-#include "Util.hpp"
-
-#endif // _ASTRO_H_
