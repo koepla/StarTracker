67dfa6cbb0b1b2453b6d3aa7a879cfa826ff1868
Elias Engelbert Plank <40146720180223@litec.ac.at>
2022-02-25 14:10:27 +0100

    added texture support

--- src/StarTracker/StarTracker/src/ExperimentalView.cpp
+++ src/StarTracker/StarTracker/src/ExperimentalView.cpp
@@ -1,7 +1,5 @@
 #include "ExperimentalView.hpp"
 
-#include <glm/glm.hpp>
-
 namespace StarTracker {
 
 	ExperimentalView::ExperimentalView(void* nativeWindowHandle) noexcept : Core::View{ nativeWindowHandle } {
@@ -14,13 +12,14 @@ namespace StarTracker {
 		vertexBuffer = std::make_shared<Core::OpenGL::VertexBuffer>();
 		indexBuffer = std::make_shared<Core::OpenGL::IndexBuffer>();
 		shader = std::make_shared<Core::OpenGL::Shader>();
+		texture = std::make_shared<Core::OpenGL::Texture>();
 
-		static std::array<glm::vec3, 4> vertices = {
+		static std::array<glm::vec3, 8> vertices = {
 
-			glm::vec3{ -0.5f, -0.5f, 0.0f },
-			glm::vec3{  0.5f, -0.5f, 0.0f },
-			glm::vec3{  0.5f,  0.5f, 0.0f },
-			glm::vec3{ -0.5f,  0.5f, 0.0f }
+			glm::vec3{ -0.5f, -0.5f, 0.0f }, glm::vec3{ 0.0f, 0.0f, 0.0f },
+			glm::vec3{  0.5f, -0.5f, 0.0f }, glm::vec3{ 1.0f, 0.0f, 0.0f },
+			glm::vec3{  0.5f,  0.5f, 0.0f }, glm::vec3{ 1.0f, 1.0f, 0.0f },
+			glm::vec3{ -0.5f,  0.5f, 0.0f }, glm::vec3{ 0.0f, 1.0f, 0.0f }
 		};
 
 		static std::array<std::uint32_t, 6> indices = {
@@ -30,34 +29,35 @@ namespace StarTracker {
 
 		static std::vector<Core::OpenGL::BufferElement> vertexBufferElements = {
 
-			Core::OpenGL::BufferElement{ Core::OpenGL::ShaderDataType::Float3, "aPosition" }
+			Core::OpenGL::BufferElement{ Core::OpenGL::ShaderDataType::Float3, "aPosition" },
+			Core::OpenGL::BufferElement{ Core::OpenGL::ShaderDataType::Float3, "aTexCoords" },
 		};
 
 		Core::OpenGL::BufferLayout vertexBufferLayout{ vertexBufferElements };
 
 		vertexArray->Bind();
-
 		vertexBuffer->SetLayout(vertexBufferLayout);
 		vertexBuffer->SetData(vertices.data(), static_cast<std::uint32_t>(vertices.size() * sizeof(glm::vec3)));
-
 		indexBuffer->SetData(indices.data(), static_cast<std::uint32_t>(indices.size()));
-	
 		vertexArray->SetIndexBuffer(indexBuffer);
 		vertexArray->SetVertexBuffer(vertexBuffer);
 
-		shader->LoadFromFile("assets/shaders/defaultVertex.glsl", "assets/shaders/defaultFragment.glsl");
+		shader->LoadFromFile("assets/shaders/textureVertex.glsl", "assets/shaders/textureFragment.glsl");
+		texture->LoadFromFile("assets//textures/pillarsOfCreation.jpg");
 	}
 
 	void ExperimentalView::OnUpdate(float deltaTime) noexcept {
 
 		shader->Bind();
+		texture->Bind(0);
 		vertexArray->Bind();
 		glDrawElements(GL_TRIANGLES, indexBuffer->GetIndexCount(), GL_UNSIGNED_INT, nullptr);
 
 		// Called every frame
 		if (ImGui::Begin("Experimental")) {
 
-			
+			const auto textureStats = std::format("Width: {}, Height: {}, Channels: {}", texture->GetWidth(), texture->GetHeight(), texture->GetChannels());
+			ImGui::Text("Texture - %s", textureStats.c_str());
 		}
 		ImGui::End();
 	}
